2b0c7b8796448df4b9d998fa53aeaa027e396e56
seb77410 <seb_perso@hotmail.fr>
2020-03-09 20:20:18 +0100

    Add method into Utils.class

--- app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.realestatemanager;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 
 import java.text.DateFormat;
@@ -23,6 +25,11 @@ public class Utils {
         return (int) Math.round(dollars * 0.812);
     }
 
+    //=====> Correction
+    public static int convertEuroToDollar(int euro){
+        return (int) Math.round(euro * 1.231);
+    }
+
     /**
      * Conversion de la date d'aujourd'hui en un format plus approprié
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
@@ -30,6 +37,12 @@ public class Utils {
      */
     public static String getTodayDate(){
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        return dateFormat.format(new Date());
+    }
+
+    //=====> Correction
+    public static String getTodayDateNew(){
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return dateFormat.format(new Date());
     }
 
@@ -43,4 +56,18 @@ public class Utils {
         WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
         return wifi.isWifiEnabled();
     }
+
+    //=====> Correction
+    public static Boolean isInternetAvailableNew(Context context){
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo.isConnected();
+        }
+        else { return false;}
+    }
+
 }
